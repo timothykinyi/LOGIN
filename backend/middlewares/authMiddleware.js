3ead72d07f78bf72ecfa56c8a0f65c569647b1b2
tee <tee@Kinyi.Kinyi>
2024-09-08 21:20:23 +0300

    all

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -2,24 +2,25 @@ const jwt = require('jsonwebtoken');
 require('dotenv').config();
 
 const authMiddleware = (req, res, next) => {
-  // Get the token from the request header
   const authHeader = req.header('Authorization');
-
-  // Check if the Authorization header exists
+  
   if (!authHeader || !authHeader.startsWith('Bearer ')) {
     return res.status(401).json({ msg: 'No token, authorization denied' });
   }
 
-  // Remove 'Bearer ' from the token string
   const token = authHeader.replace('Bearer ', '');
-
+  
   try {
-    // Verify the token
+    // Decode the token and attach the user to the request object
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    req.user = decoded; // Attach the decoded user data to the request object
-    next(); // Proceed to the next middleware or route handler
+    req.user = decoded;
+
+    // You can return the decoded token data here if necessary for the current route
+    return res.json({ user: decoded });
+    
+    next();
   } catch (error) {
-    res.status(401).json({ msg: 'Token is not valid' });
+    return res.status(401).json({ msg: 'Token is not valid' });
   }
 };
 
