47ca77a6d268b3b4b6cef3dabf359d2e2ce327e8
tee <tee@Kinyi.Kinyi>
2024-09-21 14:26:51 +0300

    all

--- backend/models/alleID.js
+++ backend/models/alleID.js
@@ -4,8 +4,7 @@ const mongoose = require('mongoose');
 const eIDSchema = new mongoose.Schema({
   eID: {
     type: String,
-    required: true, // Ensure eID is required
-    unique: true,   // Ensure eID is unique
+    required: true,
   },
 });
 
