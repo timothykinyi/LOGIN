b58a3050d0b95c52929cea948949b09a3b7d84eb
tee <tee@Kinyi.Kinyi>
2024-09-25 14:55:53 +0300

    all

--- backend/routes/dooridroutes.js
+++ backend/routes/dooridroutes.js
@@ -6,9 +6,9 @@ const DIDController = require('../controllers/dooridcontroller');
 router.get('/allowed-dids', DIDController.getAllowedDIDs);
 
 // Route to add a new door ID
-router.post('/allowed-dids', DIDController.addDID);
+router.post('/allowed-dids', DIDController.addDoorID);
 
 // Route to delete a door ID by its ID
-router.delete('/allowed-dids/:id', DIDController.removeDID);
+router.delete('/allowed-dids/:id', DIDController.removeDoorID);
 
 module.exports = router;
