2b8690662b162a6ec5d351d343d5949773063d97
tee <tee@Kinyi.Kinyi>
2024-10-19 20:41:02 +0300

    all

---
+++ pwd/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react'
+import About from './Components/Navbar/About/About'
+import Hero from './Components/Navbar/Hero/Hero'
+import Navbar from './Components/Navbar/Navbar'
+
+
+
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Hero/>
+      <About/>
+    </div>
+  )
+}
+
+export default App
