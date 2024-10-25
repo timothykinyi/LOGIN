3c7f9f6a862efeabd67fa41a45de2ac4ddfb833e
tee <tee@Kinyi.Kinyi>
2024-10-25 16:08:01 +0300

    all

--- backend/routes/firebaseConfig.js
+++ backend/routes/firebaseConfig.js
@@ -1,6 +1,6 @@
 // server/firebaseConfig.js
 const admin = require("firebase-admin");
-const serviceAccount = require("./own-my-data-firebase-adminsdk-ufawq-eb06ad3795.json");
+const serviceAccount = require("./eidltd-863565558301.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
